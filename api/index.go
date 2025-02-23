e54ee536241ce00054bf91fe0b2e44d116e1df2d
Martin Enderlein <sahel139@googlemail.com>
2025-02-23 20:35:36 +0100

    add clerk middleware

--- api/index.go
+++ api/index.go
@@ -1,10 +1,29 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/clerk/clerk-sdk-go/v2"
+	"github.com/clerk/clerk-sdk-go/v2/user"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go there. now!</h1>")
+	ctx := r.Context()
+	claims, ok := clerk.SessionClaimsFromContext(ctx)
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"access": "unauthorized"}`))
+		return
+	}
+
+	usr, err := user.Get(ctx, claims.Subject)
+	if err != nil {
+		panic(err)
+	}
+	if usr == nil {
+		w.Write([]byte("User does not exist"))
+		return
+	}
+
+	w.Write([]byte("Hello " + *usr.FirstName))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module projekt-splazh
 
 go 1.23.0
+
+require (
+	github.com/clerk/clerk-sdk-go/v2 v2.2.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+)
